b4ec46b3a89f7047fb1dad4f047f28bc85b65c1b
olivia-loup <olivia.loup@heig-vd.ch>
2024-10-15 16:49:27 +0200

    clignote

---
+++ BUILD/DISCO_H747I/ARMC6/mbed-os/platform/source/newlib_nano_malloc_workaround.d
@@ -0,0 +1,3 @@
+BUILD\DISCO_H747I\ARMC6\mbed-os\platform\source\newlib_nano_malloc_workaround.o: \
+  mbed-os\platform\source\newlib_nano_malloc_workaround.c \
+  BUILD\DISCO_H747I\ARMC6\mbed_config.h
---
+++ BUILD/DISCO_H747I/ARMC6/mbed-os/targets/TARGET_STM/ospi_api.d
@@ -0,0 +1,3 @@
+BUILD\DISCO_H747I\ARMC6\mbed-os\targets\TARGET_STM\ospi_api.o: \
+  mbed-os\targets\TARGET_STM\ospi_api.c \
+  BUILD\DISCO_H747I\ARMC6\mbed_config.h
