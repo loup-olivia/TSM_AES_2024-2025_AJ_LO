88712899cae3745d9c92f26e24f635a4cf66ca1b
olivia-loup <olivia.loup@heig-vd.ch>
2024-10-22 00:05:08 +0200

    check pre-commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "bike_system.hpp"
+#include "constants.hpp"
 #include "mbed.h"
 #include "mbed_trace.h"
-#include "constants.hpp"
-#include "bike_system.hpp"
-
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE 500ms
@@ -16,15 +15,14 @@
 #define TRACE_GROUP "MAIN"
 #endif  // MBED_CONF_MBED_TRACE_ENABLE
 
-
 #if !MBED_TEST_MODE
 int main() {
     // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led = false;
-#endif
+    // #ifdef LED1
+    //     DigitalOut led(LED1);
+    // #else
+    //     bool led = false;
+    // #endif
 
 #if defined(MBED_CONF_MBED_TRACE_ENABLE)
     mbed_trace_init();
@@ -36,4 +34,3 @@ int main() {
     }
 }
 #endif
-
